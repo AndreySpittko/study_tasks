4dd31dfc2636fc9ed57a42d858fe7271465777db
tymchenko <tymchenkolviv@gmail.com>
2018-07-14 21:35:00 +0300

    updated ExchangeService

--- src/exchangeService/main/ExchangeService.java
+++ src/exchangeService/main/ExchangeService.java
@@ -1,4 +1,42 @@
 package exchangeService.main;
 
+
+import exchangeService.services.FileUtil;
+import exchangeService.services.JsonService;
+import exchangeService.services.StringConverter;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+
 public class ExchangeService {
+    private final static String URL = "https://exchangeratesapi.io/api/latest";
+
+    public static void main(String[] args) throws IOException {
+        disableSSL();
+        JsonService service = new JsonService();
+        new FileUtil().write(new StringConverter().convert(service.readJson(URL)));
+    }
+
+    private static void disableSSL() {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+        }};
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/exchangeService/services/FileUtil.java
+++ src/exchangeService/services/FileUtil.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class FileUtil {
     //постав правильну лінку папку куди зберігати файл
-    private static final String FILE_NAME = "/Users/ivan/Documents/projects/Andrii/src/main/resources/exchange_rate.txt";
+    private static final String FILE_NAME = "/Users/ivan/Documents/projects/Andrii_study/src/exchangeService/resouces/exchage_rates_json.txt";
 
     public void write(String string){
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_NAME))) {
